d0654406ad4209cee954fe27151ba0743f44401e
denxram <den.xram@mail.ru>
2021-02-07 00:34:06 +0300

    изменил header  и burger.js

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,9 +1,12 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const logo = document.querySelector('.main-nav__logo-image');
 
 navMain.classList.remove('main-nav--nojs');
 
-navToggle.onclick = function () {
-  navMain.classList.toggle('main-nav--closed');
+document.querySelector('.main-nav__toggle').addEventListener('click', function () {
   navMain.classList.toggle('main-nav--opened');
-};
+  logo.classList.toggle('main-nav__logo-image--active');
+});
+
+
+
